898841cbf2cdfbe7f6e431e3dfafc6b2346921a6
ismo007-kali <lawaliismail597@gmail.com>
2025-08-12 21:09:45 +0100

    ismail

---
+++ contact.php
@@ -0,0 +1,97 @@
+<?php
+require_once 'header.php'
+?>
+
+  <!-- end contact section -->
+  <!-- info section -->
+<section class="info_section  layout_padding2-top">
+    <div class="social_container">
+      <div class="social_box">
+        <a href="https://www.facebook.com/modetendanceny?mibextid=ZbWKwL" target="_blank">
+          <i class="fa fa-facebook" aria-hidden="true"></i>
+        </a>
+        <a href="https://wa.me/+22789735585" target="_blank">
+          <i class="fa fa-whatsapp" aria-hidden="true"></i>
+        </a>
+        <a href="https://www.instagram.com/modetendanceny" target="_blank">
+          <i class="fa fa-instagram" aria-hidden="true"></i>
+        </a>
+
+      </div>
+    </div>
+    <div class="info_container ">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 col-lg-3">
+            <h6>
+              A PROPOS
+            </h6>
+            <p>
+              Chez MODE ET TENDANCE,Nous vous proposons une large gamme de sacs,montres ,et bien plus encore,conçus pour ajouter une touches d’élégance  à votre look. 
+
+            </p>
+          </div>
+          <div class="col-md-6 col-lg-3">
+            <div class="info_form ">
+              <h5>
+                <strong>MESSAGES</strong>
+              </h5>
+              <p>Envoyez un e-mail à la boutique
+                pour plus d'informations ou passez<br>
+                une commande
+              </p>
+              <form action="mailto:modetendanceny">
+                <button>
+                  Ecrire
+                </button>
+              </form>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3">
+            <h6>
+              BESOIN D'AIDE ?
+            </h6>
+            <p>
+              Vous cherchez un produit en quantité et qui n'est pas disponible.Vous pouvez passer une commande direct.Il vous sera remis dès son arrivée
+            </p>
+          </div>
+          <div class="col-md-6 col-lg-3">
+            <h6>
+              CONTACTEZ NOUS
+            </h6>
+            <div class="info_link-box">
+              <a>
+                <i class="fa fa-map-marker" aria-hidden="true"></i>
+                <span>Qtr Ryad Plaque Avocat </span>
+              </a>
+              <a>
+                <i class="fa fa-phone" aria-hidden="true"></i>
+                <span>+227 89 73 55 85 </span>
+              </a>
+              <a href="mailto:modetendanceny@gmail.com">
+                <i class="fa fa-envelope" aria-hidden="true"></i>
+                <span>modetendanceny@gmail.com</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+    </footer>
+    <!-- footer section -->
+
+  </section>
+
+  <!-- end info section -->
+
+
+  <script src="js/jquery-3.4.1.min.js"></script>
+  <script src="js/bootstrap.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js">
+  </script>
+  <script src="js/custom.js"></script>
+
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -1,85 +1,6 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-  <!-- Basic -->
-  <meta charset="utf-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <!-- Mobile Metas -->
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-  <!-- Site Metas -->
-  <meta name="keywords" content="" />
-  <meta name="description" content="" />
-  <meta name="author" content="" />
-  <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
-  <title>MODE ET TENDANCE</title>
-  <!-- slider stylesheet -->
-  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
-
-  <!-- bootstrap core css -->
-  <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
-
-  <!-- Custom styles for this template -->
-  <link href="css/style.css" rel="stylesheet" />
-  <!-- responsive style -->
-  <link href="css/responsive.css" rel="stylesheet" />
-  
-</head>
-
-<body>
-  <div class="hero_area">
-    <!-- header section strats -->
-    <header class="header_section">
-      <nav class="navbar navbar-expand-lg custom_nav-container ">
-        <a class="navbar-brand" href="index.html">
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class=""></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav  ">
-            <li class="nav-item active">
-              <a class="nav-link" href="index.html"><strong>ACCUEIL</strong> <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="shop.html">
-                <strong>Shop</strong>
-              </a>
-            </li>
-          <!--   <li class="nav-item">
-              <a class="nav-link" href="why.html">
-               <strong>POURQUOI NOUS</strong>
-              </a>
-            </li> -->
-            <li class="nav-item">
-              <a class="nav-link" href="testimonial.html">
-                <strong>TEMOIGNAGE</strong>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="contact.html"><strong>CONTACT</strong></a>
-            </li>
-          </ul>
-          <div class="user_option">
-            <a href="">
-              <i class="fa fa-upser" aria-hidden="true"></i>
-              <span>
-                
-              </span>
-            </a>
-            <a href="">
-              <i class="fa fa-shoppiong-bag" aria-hidden="true"></i>
-            </a>
-           <!--  <form class="form-inline ">
-              <button class="btn nav_search-btn" type="submit">
-                <i class="fa fa-search" aria-hidden="true"></i>
-              </button>
-            </form> -->
-          </div>
-        </div>
-      </nav>
-    </header>
+<?php
+require_once 'header.php';
+?>
     <!-- end header section -->
     <!-- slider section -->
     <section class="slider_section">
@@ -559,7 +480,7 @@
     </div>
   </section>
     </footer>
-    <!-- footer section -->
+    
 
   </section>
 
